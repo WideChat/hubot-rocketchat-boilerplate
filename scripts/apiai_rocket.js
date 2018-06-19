bf03cc56758a4059c2b46739aa1d0b86667170fd
erosenthal <eric.rosenthal@viasat.com>
2018-06-19 10:27:00 -0400

    update bot.js scripts

--- scripts/apiai_rocket.js
+++ scripts/apiai_rocket.js
@@ -1,3 +1,16 @@
+/*
+export ROCKETCHAT_URL=export ROCKETCHAT_URL=https://ear-hotchat2.herokuapp.com:443
+export ROCKETCHAT_ROOM=''
+export LISTEN_ON_ALL_PUBLIC=false
+export RESPOND_TO_DM=true
+export ROCKETCHAT_PASSWORD=vera
+export ROCKETCHAT_AUTH=password
+export API_AI_CLIENT_ACCESS_TOKEN=158f65db537f45c8aa94e3a92f913438
+export ROCKETCHAT_USESSL=true
+export ROCKETCHAT_USER=vera
+*/
+
+
 var apiai = require('apiai');
 var app = apiai(process.env.API_AI_CLIENT_ACCESS_TOKEN);
 
@@ -14,26 +27,27 @@ module.exports = (robot) => {
             //console.log(response.result.fulfillment.speech);
             console.log(response.result.metadata.intentName);
 
+            // Text
+            if (response.result.fulfillment.speech) {
+                msg.send(response.result.fulfillment.speech);
+            }
+
             var messages = response.result.fulfillment.messages;
-            // Attachments and richMessages
+            // rich messages
             for (var i = 0; i < messages.length; i++) {
                 if (messages[i].payload && messages[i].payload.attachments) {
                     robot.adapter.customMessage({
                         rid: msg.message.user["roomID"],
-                        attachments: messages[i].payload.attachments
+                        attachments: messages[i].payload.attachments,
                     })
                 }
-                // Rich messages
-                if (messages[i].payload && messages[i].payload.richMessage) {
+                // Rich messages deprecated
+                if (messages[i].payload && messages[i].payload.type) {
                     robot.adapter.customMessage({
                         rid: msg.message.user["roomID"],
-                        richMessage: messages[i].payload.richMessage
+                        payload: messages[i].payload
                     })
                 }
-            }
-            // Text
-            if (response.result.fulfillment.speech) {
-                msg.send(response.result.fulfillment.speech);
             }
 
         });
@@ -45,3 +59,4 @@ module.exports = (robot) => {
         request.end();
     })
 }
+
---
+++ scripts/yandex-translator.js
@@ -0,0 +1,69 @@
+'use strict';
+
+function yandexTranslate (robot) {
+  var pattern = '(?!(@[a-z]*)) (.*)'
+  var rparts = new RegExp(pattern, 'i');
+
+  robot.respond(rparts, request);
+
+  function request (command) {
+    var input = command.match[2].trim();
+
+    // Query to detect which language the message is in, either spanish or english
+    var q_detect = {
+      key: process.env.YANDEX_TRANSLATE_API_KEY,
+      text: input,
+      hint: ['es', 'en']
+    };
+
+    command
+      .http('https://translate.yandex.net/api/v1.5/tr.json/detect')
+      .query(q_detect)
+      .header('User-Agent', 'Mozilla/5.0')
+      .get()(detect);
+
+    function detect (err, res, body) {
+      var parsed = parse(body);
+      if (parsed) {
+        console.log("this is the parsed body: " + JSON.stringify(parsed));
+        var origin = parsed.lang;
+      } else {
+        command.send("Sorry, could not detect the language of origin.");
+      }
+      var target = ( origin == 'en' ? 'es' : 'en');
+      var lang = origin + '-' + target
+      var q = {
+        key: process.env.YANDEX_TRANSLATE_API_KEY,
+        lang: lang,
+        text: input
+      };
+
+      command
+        .http('https://translate.yandex.net/api/v1.5/tr.json/translate')
+        .query(q)
+        .header('User-Agent', 'Mozilla/5.0')
+        .get()(response);
+    }
+
+    //var origin = 'en';
+    //var target = 'es';
+
+    function response (err, res, body) {
+      var parsed = parse(body);
+      if (parsed) {
+        command.send(parsed.text);
+      }
+    }
+  }
+}
+
+function parse (json) {
+  try {
+    return JSON.parse(json);
+  } catch (e) {
+    return { text: 'No.' };
+  }
+}
+
+module.exports = yandexTranslate;
+
